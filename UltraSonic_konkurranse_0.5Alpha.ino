78935e2ecceb93960d5979b63740f452609e94c8
marius_hesselberg <hesmar15@student.westerdals.no>
2017-03-14 17:00:58 +0100

    Beta 0.9 UltraSonic_Konkurranse

--- UltraSonic_konkurranse_0.5Alpha.ino
+++ UltraSonic_konkurranse_0.5Alpha.ino
@@ -4,56 +4,75 @@ MeDCMotor motor1(M1);
 MeDCMotor motor2(M2);
 MeUltrasonicSensor ultraSensor(PORT_3);
  
-int baseSpeedMoter1 = 100;
-int baseSpeedMotor2 = -100;
+int baseSpeedMotor1= 200;
+int baseSpeedMotor2 = -200;
  
-int rightTurnMotor1 = -75;
-int rightTurnMotor2 = -75;
-int leftTurnMotor1 = 75;
-int leftTurnMotor2 = 75;
+int rightTurnMotor1 = -90;
+int rightTurnMotor2 = -90;
+int leftTurnMotor1 = 90;
+int leftTurnMotor2 = 90;
  
 double distance;
 double currentDistanceRight;
 double currentDistanceLeft;
 
 void turnRight() {
-  motor1.run(rightTurnMotor1);
-  motor2.run(rightTurnMotor2);
-  delay(1500);
+  motor1.run(rightTurnMotor1*1.5);
+  motor2.run(rightTurnMotor2*1.5);
+  delay(330);
 }
 
 void turnLeft() {
-  motor1.run(leftTurnMotor1);
-  motor2.run(leftTurnMotor2);
-  delay(800);
+  motor1.run(leftTurnMotor1*1.5);
+  motor2.run(leftTurnMotor2*1.5);
+  delay(330);
+}
+
+void backup() {
+  Serial.println("RYGGER");
+  motor1.run(-200);
+  motor2.run(200);
+  delay(150);
 }
 
 void checkRight() {
-  motor1.run(rightTurnMotor1);
-  motor2.run(rightTurnMotor2);
-  delay(1500);
-  currentDistanceRight = calculateDistance();
+  int tempCalculation = 0;
+  for(int i = 0; i < 10; i++) {
+    motor1.run(rightTurnMotor1);
+    motor2.run(rightTurnMotor2);
+    delay(33);
+    tempCalculation = tempCalculation + calculateDistance();
+  }
+  currentDistanceRight = tempCalculation/10;
+  Serial.println("HØYRE: ");
+  Serial.print(currentDistanceRight);
   resetLeft();
 }
  
 void checkLeft() {
-  motor1.run(leftTurnMotor1);
-  motor2.run(leftTurnMotor2);
-  delay(1500);
-  currentDistanceLeft = calculateDistance();
+  int tempCalculation = 0;
+  for(int i = 0; i < 10; i++) {
+     motor1.run(leftTurnMotor1);
+     motor2.run(leftTurnMotor2);
+    delay(33);
+    tempCalculation = tempCalculation + calculateDistance();
+  }
+  currentDistanceLeft = tempCalculation/10;
+  Serial.println("VENSTRE: ");
+  Serial.print(currentDistanceLeft);
   resetRight();
 }
 
 void resetRight() {
   motor1.run(rightTurnMotor1);
   motor2.run(rightTurnMotor2);
-  delay(1500);
+  delay(330);
 }
 
 void resetLeft() {
   motor1.run(leftTurnMotor1);
   motor2.run(leftTurnMotor2);
-  delay(1500);
+  delay(330);
 }
  
 double calculateDistance() {
@@ -61,9 +80,12 @@ double calculateDistance() {
 }
  
 bool checkTurnDirection() {
-  if(currentDistanceRight < currentDistanceLeft) {
+  Serial.println("VELGER ");
+  if(currentDistanceRight > currentDistanceLeft) {
+    Serial.print("HØYRE");
     return true;
   }
+  Serial.print("VENSTRE");
   return false;
 }
  
@@ -71,11 +93,10 @@ void logDistance() {
   Serial.print("Distance : ");
   Serial.print(ultraSensor.distanceCm() );
   Serial.println(" cm");
-  delay(100);
 }
 
 void forward() {
-  motor1.run(baseSpeedMoter1); /* value: between -255 and 255. */
+  motor1.run(baseSpeedMotor1); /* value: between -255 and 255. */
   motor2.run(baseSpeedMotor2); /* value: between -255 and 255. */
   delay(50); 
 }
@@ -85,14 +106,15 @@ void setup() {
 }
  
 void loop() {
-  logDistance();
+  //logDistance();
   double x = calculateDistance();
   x = x + 0.5;
   int distance = (int)x;
-  if(distance < 15) {
+  if(distance <= 21) {
+      backup();
       checkLeft();
       checkRight();
-      if(checkTurnDirection) {
+      if(checkTurnDirection()) {
           turnRight();
       } else {
           turnLeft();
